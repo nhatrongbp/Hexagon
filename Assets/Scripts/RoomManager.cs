b9fd1f85bb63048a864a333cbe2e3e2ef74cfd41
nhatrongbp <dotrungtuann@gmail.com>
2024-07-16 22:50:35 +0700

    import photon pun 2

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("joined lobby");
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+    }
+}
